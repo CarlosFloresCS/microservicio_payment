11f09a3c8b15a9bcfb86f495a4e77ad037fbe040
CarlosFloresCS <carlos.flores.p@utec.edu.pe>
2023-05-03 06:53:16 -0500

    migracion

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,15 @@ from flask import Flask
 from config import Config
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from app.topRoutes import bp
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
-    
     db.init_app(app)
+    migrate = Migrate(app,db,compare_type=True)
 
     from app.models.card import Card
     from app.models.user import User
@@ -20,5 +20,7 @@ def create_app(config_class=Config):
         db.create_all()
     
     from app import topRoutes
+    from app.controllers.payments_controller import payments_bp
     app.register_blueprint(topRoutes.bp)
+    app.register_blueprint(payments_bp)
     return app
