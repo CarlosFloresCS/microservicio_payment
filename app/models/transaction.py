98051c17382892773626d8d1355faa48c79e7819
CarlosFloresCS <carlos.flores.p@utec.edu.pe>
2023-05-03 06:54:42 -0500

    agregando dict y reembolso en transaction

--- app/models/transaction.py
+++ app/models/transaction.py
@@ -9,6 +9,7 @@ class TransactionState(Enum):
     PENDING = "PENDING"
     APPROVED = "APPROVED"
     DECLINED = "DECLINED"
+    REFUNDED = "REFUNDED"
 
 class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -17,6 +18,16 @@ class Transaction(db.Model):
     date_hour = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     mount = db.Column(db.Float, nullable=False)
     state = db.Column(db.Enum(TransactionState), default=TransactionState.PENDING)
+
+    def as_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'card_id': self.card_id,
+            'date_hour': self.date_hour.strftime('%Y-%m-%d %H:%M:%S'),
+            'mount': self.mount,
+            'state': self.state.name
+        }
 
     def __repr__(self):
         return '<Transaction {}>'.format(self.id)
